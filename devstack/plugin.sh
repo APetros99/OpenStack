b5779aed90cf9f4066467f458e952b60ffea0804
APetros99 <115032731+APetros99@users.noreply.github.com>
2024-11-18 12:55:35 +0100

    creato plugin_configure

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,12 +1,17 @@
 function plugin_install {
     echo "Installazione di Kubernetes"
 
-    # Installazione di kubectl
     sudo curl -LO $KUBECTL_INSTALL_URL
     sudo chmod +x kubectl
     sudo mv kubectl /usr/local/bin/
 
-    # Installazione di kubeadm e kubelet
     sudo apt-get update
     sudo apt-get install -y kubeadm kubelet kubernetes-cni
+}
+
+function plugin_configure {
+    echo "Configurazione di Kubernetes con OpenStack"
+
+    openstack network create kubernetes-network
+    openstack subnet create --network kubernetes-network --subnet-range 192.168.1.0/24 kubernetes-subnet
 }
