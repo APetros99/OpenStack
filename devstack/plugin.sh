479a5370094794307a025f3ad68907fa1a3aab33
APetros99 <115032731+APetros99@users.noreply.github.com>
2024-11-18 13:02:07 +0100

    creato plugin_start

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -15,3 +15,15 @@ function plugin_configure {
     openstack network create kubernetes-network
     openstack subnet create --network kubernetes-network --subnet-range 192.168.1.0/24 kubernetes-subnet
 }
+
+function plugin_start {
+    echo "Avvio del cluster Kubernetes"
+
+    openstack server create --image <image-id> --flavor <flavor-id> \
+	--network kubernetes-network --key-name <key-name> master-node
+    for i in $(seq 1 $K8S_NODE_COUNT); do
+	openstack server create --image<image-id> --flavor <flavor-id> \
+	    --network kubernetes-network --key-name <key-name> worker-node-$i
+    done
+
+}
